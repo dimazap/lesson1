77f3ab6280bfe5f91d54a1a88877c17ac3cdfa0e
Dima Zaplavsky <dima.zap@gmail.com>
2018-11-28 12:44:05 +0100

    Telegram Bot upload

---
+++ bot.py
@@ -0,0 +1,36 @@
+# Импортируем нужные компоненты
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import logging
+import settings
+
+
+
+logging.basicConfig(format='%(ascdate)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+
+def main():
+    mybot = Updater(settings.API_KEY)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    mybot.start_polling()
+    mybot.idle()
+
+main()
