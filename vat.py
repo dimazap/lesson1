5bdcef898c0023c961787483bf48748d3a659f1d
Dima Zaplavsky <dima.zap@gmail.com>
2018-12-02 13:14:51 +0100

    Spaces added to vat.py; script finally works :); changed order of lines 6 and 5 to make it work correctly

--- vat.py
+++ vat.py
@@ -1,12 +1,12 @@
-def get_vat(payment, percent=19):
+def get_vat(payment, percent):
 	try:
-	payment = float(payment)
-	percent = int(percent)
-	vat = round(vat, 2)
-	vat = payment / 100 * percent
-	return "VAT amount: {}".format(vat)
-except (TypeError, ValueError):
-	return "Can't calculate"
+		payment = float(payment)
+		percent = int(percent)
+		vat = payment / 100 * percent
+		vat = round(vat, 2)
+		return f"VAT amount: {vat}"
+	except (TypeError, ValueError):
+		return "Can't calculate"
 
-result = get_vat(400, "20")
+result = get_vat(400, "19")
 print(result)
